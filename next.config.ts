9facef44168c5d61fc1b7b285c70a83510804672
zkxp-admin <info@zkxp.xyz>
2025-10-02 19:39:36 -0600

    Remove mantine hooks package optimization

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ['@mantine/hooks'],
-  }
 };
 
 export default nextConfig;
